6123ed61acd12c5ee788348044a2bfaab4782e40
Henzo Araujo <henzomartinsaraujo19@gmail.com>
2026-08-13 16:23:04 -0300

    Design Popup

---
+++ src/Popup.tsx
@@ -0,0 +1,22 @@
+function Popup() {
+  return (
+    
+    <div className="h-100 w-100 flex flex-col">
+
+      <div className="bg-[#16161a] h-2/7 flex justify-center">
+        
+        <img className="h-5/4 relative bottom-2 drop-shadow-[0_2px_5px_rgba(34,197,94,1)] drop-shadow-[0_5px_12px_rgba(34,197,94,0.9)] drop-shadow-[0_10px_25px_rgba(22,163,74,0.7)]" src="images/title.png" alt="Lenex" />
+
+      </div>
+
+      <div className="bg-[#101013] h-5/7 border-t border-[#2b2b2e]">
+
+
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Popup

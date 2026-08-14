37e504b5f3a7d743ddcd0d8c374061abd0f999b4
Henzo Araujo <henzomartinsaraujo19@gmail.com>
2026-08-14 10:49:05 -0300

    Alterações Popup

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -1,7 +1,7 @@
 function Popup() {
   return (
     
-    <div className="h-100 w-100 flex flex-col">
+    <div className="h-120 w-90 flex flex-col">
 
       <div className="bg-[#16161a] h-2/7 flex justify-center">
         
@@ -9,9 +9,19 @@ function Popup() {
 
       </div>
 
-      <div className="bg-[#101013] h-5/7 border-t border-[#2b2b2e]">
+      <div className="bg-[#101013] font-roboto flex justify-center items-center p-4 text-base h-5/7 border-t border-[#2b2b2e]">
 
+        <h2 className="text-white absolute top-1/3 border-b w-full text-center pb-4 border-[#2b2b2e]">Acesse sua conta:</h2>
 
+        <form className="flex flex-col gap-2 w-3/4 text-sm" action="">
+
+          <input className="bg-white p-2" type="email" placeholder="Insira seu email" />
+          <input className="bg-white p-2" type="password" placeholder="Insira sua senha" />
+          <button className="bg-green-500 font-bebas tracking-wider text-xl p-1.5 cursor-pointer shadow-green-800 shadow-[0_20px_80px_-5px_rgba(0,0,0,0.4)]  hover:bg-green-600 hover:scale-105 transition font-bold text-white" type="submit">LOGIN</button>
+        
+        </form>
+
+        <h2 className="text-white absolute bottom-4 flex flex-row justify-center gap-1 border-t w-full text-center pt-4 border-[#2b2b2e]">Versão<p className="text-green-600">1.0.0</p></h2>
 
       </div>
 
